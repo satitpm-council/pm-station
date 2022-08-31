19b8cc182d966a393ff905ad70dc61ab313afcb6
Lemasc Service <lemascserviceth@hotmail.com>
2022-08-31 10:32:38 +0700

    favicon

--- apps/client-web/app/root.tsx
+++ apps/client-web/app/root.tsx
@@ -12,7 +12,7 @@ import styles from "./styles/root.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "coolkidssatit",
   viewport: "width=device-width,initial-scale=1",
 });
 
