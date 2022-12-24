306106cbac66963a78e1d51bff11d1aec38a22a7
Lemasc Service <lemascserviceth@hotmail.com>
2022-12-24 20:33:38 +0700

    Use existing sentry.client file

--- apps/client-web/sentry.js
+++ apps/client-web/sentry.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 require("dotenv").config();
 require("dotenv").config({ path: "../../.env" });
-if (!process.env.SENTRY_DSN) {
+
+const sentryClientFile = path.join(process.cwd(), "app", "sentry.client.json");
+if (!process.env.SENTRY_DSN && !fs.existsSync(sentryClientFile)) {
   throw new Error("SENTRY_DSN environment varriable not found.");
 }
 fs.writeFileSync(
-  path.join(process.cwd(), "app", "sentry.client.json"),
+  sentryClientFile,
   JSON.stringify({ SENTRY_DSN: process.env.SENTRY_DSN })
 );
