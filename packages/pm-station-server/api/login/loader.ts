bd8eb3c6db013d2b56d3d1e951447cdb65e1a77b
Lemasc Service <lemascserviceth@hotmail.com>
2022-12-28 22:41:40 +0700

    Fix dependencies after pruned

--- packages/pm-station-server/api/login/loader.ts
+++ packages/pm-station-server/api/login/loader.ts
@@ -1,7 +1,7 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { logoutSession, verifySession } from "@station/server/auth/remix";
+import { logoutSession, verifySession } from "@station/server/auth";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const session = await verifySession(request);
