14e54a2c61edd9c3bc9d77d78b78ba12b11e78ea
Lemasc <lemascserviceth@hotmail.com>
2023-01-09 10:38:35 +0000

    Bottom sheet

--- apps/kiosk-web/app/controller/app/components/SongSelector.tsx
+++ apps/kiosk-web/app/controller/app/components/SongSelector.tsx
@@ -1,14 +1,14 @@
 "use client";
+
 import { SongRequestRecordList } from "../components/SongRequest/list";
 import { usePlaylistData } from "@station/client/playlists";
 import { controllerStore } from "../shared/store";
 
-export default function SongRequestTab() {
+export default function SongSelector() {
   const playlistId = controllerStore((state) => state.playlistId);
   const { tracks } = usePlaylistData(playlistId);
   return (
     <>
-      <h1 className="font-bold text-4xl">รายการเพลง</h1>
       <SongRequestRecordList onItemClick={console.log} data={tracks} />
     </>
   );
