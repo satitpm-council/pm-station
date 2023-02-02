5cb775e80ded7a70a28dffb13afd3d496efeadd7
Lemasc Service <lemascserviceth@hotmail.com>
2023-02-02 17:42:07 +0700

    client-web: use pythonia (no native deps)

--- apps/client-web/app/utils/pm-station/ytmusic/index.ts
+++ apps/client-web/app/utils/pm-station/ytmusic/index.ts
@@ -1,9 +1,37 @@
-import { interpreter as py } from "node-calls-python";
+import { python } from "pythonia/src/pythonia/index";
 import path from "path";
 import type { MusicInfo } from "./types";
 
 const entryPath = path.resolve(process.cwd(), "python", "ytmusic.py");
-const entry = py.importSync(entryPath);
+
+type PythonEntry = {
+  getMusicInfo: (videoId: string[]) => Promise<MusicInfo[]>;
+  searchMusic: (query: string[]) => Promise<MusicInfo[]>;
+};
+
+let entry: any;
+
+async function init() {
+  if (!entry) {
+    entry = await python(entryPath);
+  }
+}
+
+async function call<
+  K extends keyof PythonEntry,
+  R extends Awaited<ReturnType<PythonEntry[K]>>
+>(method: K, ...args: Parameters<PythonEntry[K]>): Promise<R> {
+  await init();
+  let value = await entry[method](...args);
+  if (!Array.isArray(value)) value = value.valueOf();
+  exit();
+  return value;
+}
+
+function exit() {
+  // @ts-expect-error Why they don't give the types?
+  python.exit();
+}
 
 /**
  * Search music on the YouTube Music Library and returns the first matched result.
@@ -11,7 +39,7 @@ const entry = py.importSync(entryPath);
  * @param query Search queries
  */
 export async function searchMusic(...query: string[]): Promise<MusicInfo[]> {
-  const results = (await py.call(entry, "searchMusic", query)) as MusicInfo[];
+  const results = await call("searchMusic", query);
   return results;
 }
 
@@ -30,11 +58,8 @@ export function getMusicInfo(...videoId: string[]): Promise<MusicInfo[]>;
 export async function getMusicInfo(
   ...videoId: string[]
 ): Promise<MusicInfo | MusicInfo[]> {
-  const results = (await py.call(
-    entry,
-    "getMusicInfo",
-    videoId
-  )) as MusicInfo[];
+  const results = await call("getMusicInfo", videoId);
+  console.log(results);
   if (videoId.length === 1) return results[0];
   return results;
 }
