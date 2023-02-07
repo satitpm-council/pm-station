bcfc6b268412c6b8774a8c8306af8b4ee3838243
Lemasc Service <lemascserviceth@hotmail.com>
2023-02-07 21:04:42 +0700

    kiosk-web: use global env parsing

--- apps/kiosk-web/next.config.js
+++ apps/kiosk-web/next.config.js
@@ -2,21 +2,19 @@ const path = require("path");
 const { getPackages } = require("build-config");
 const { DefinePlugin } = require("webpack");
 
-const { parsed } = require("dotenv").config({ path: "../../.env" });
+require("dotenv").config({ path: "../../.env" });
 
 // Although Next.js provides 'NEXT_PUBLIC' prefixes for env to be exposes to client
 // We just want to preserve variable names on both frameworks.
 // As a result, we implement a custom logic that will pass specific public env variables.
-const injectedEnvVars = parsed
-  ? Object.fromEntries(
-      // This seems to be vulnerable, but environments are exposed to system when building on Vercel.
-      Object.entries(parsed)
-        // For example, PM_STATION_FIREBASE(_PUBLIC_)APP_ID should pass,
-        // but SOMETHING_PUBLIC shouldn't.
-        .filter(([k]) => k.includes("_PUBLIC_"))
-        .map(([k, v]) => [`process.env.${k}`, JSON.stringify(v)])
-    )
-  : {};
+const injectedEnvVars = Object.fromEntries(
+  // This seems to be vulnerable, but environments are exposed to system when building on Vercel.
+  Object.entries(process.env)
+    // For example, PM_STATION_FIREBASE(_PUBLIC_)APP_ID should pass,
+    // but SOMETHING_PUBLIC shouldn't.
+    .filter(([k]) => k.includes("_PUBLIC_"))
+    .map(([k, v]) => [`process.env.${k}`, JSON.stringify(v)])
+);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
