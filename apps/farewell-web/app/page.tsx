1517cd0549615f76a2f389a870d0392b2659ffa4
Lemasc Service <lemascserviceth@hotmail.com>
2023-02-17 20:01:57 +0700

    farewell: processing images

--- apps/farewell-web/app/page.tsx
+++ apps/farewell-web/app/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import whiteLogo from "../public/logo/WHITE.png";
 
 export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center flex-grow bg-red-800 text-white">
       <Image
         className="-my-10"
-        src="/logo/WHITE.png"
+        src={whiteLogo}
         width={300}
         height={300}
         alt="Logo"
       />
       <h1 className="uppercase -mt-10">Photobooth</h1>
-      <span className="py-4 text-center leading-8">
+      <span className="p-4 text-center leading-8">
         ขณะนี้กำลังประมวลผลรูปถ่าย
         หากเรียบร้อยแล้วจะแจ้งผ่านไลน์กลุ่มหัวหน้าห้องอีกครั้ง
         <br />
