8b2bb2627abfc35da5ed3694711eb3cf7428de85
Lemasc Service <lemascserviceth@hotmail.com>
2023-02-18 12:51:40 +0700

    farewell: rewrites

--- apps/client-web/app/routes/qr.tsx
+++
@@ -1,7 +0,0 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-
-export const loader: LoaderFunction = async ({ request }) => {
-  const { search } = new URL(request.url);
-  return redirect(`/farewell/qr${search}`);
-};
